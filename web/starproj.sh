a9c6ee46166e32df8bebbb4114c75187b64ee674
mike o'brien <mike@meetup.com>
2009-12-01 15:27:49 -0500

    updated init script

--- web/starproj.sh
+++ web/starproj.sh
@@ -1,20 +1,24 @@
 #!/bin/sh
 
-global_settings_dir=/home/mike/projects/globalsettings
+global_settings_dir=/home/mike/projects/untitled-web-framework/globalsettings
 projectname=$1
 echo $1
 
 mkdir -p $projectname/lib
 mkdir -p $projectname/conf
 mkdir -p $projectname/tools
-mkdir -p $projectname/src
+mkdir -p $projectname/src/$projectname/ctrlrs
 mkdir -p $projectname/WebContent/WEB-INF/
 cp $global_settings_dir/lib/*.jar $projectname/lib
 cp $global_settings_dir/build.xml $projectname
 cp $global_settings_dir/log4j.properties $projectname/conf
 cp $global_settings_dir/settings.properties $projectname
 
+cp $global_settings_dir/HomeController.java $projectname/src/$projectname/ctrlrs
+cp $global_settings_dir/web.xml $projectname/WebContent/WEB-INF
+
 sed -i 's/!!!PROJECT_NAME!!!/testproject/g' $projectname/build.xml 
+sed -i 's/!!!PROJECT_NAME!!!/testproject/g' $projectname/src/$projectname/ctrlrs/HomeController.java
 
 
 
