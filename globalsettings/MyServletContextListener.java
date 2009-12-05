573d2f8e3a6db3aea1b7b6a8b959d50d17880f92
mike o'brien <mike@meetup.com>
2009-12-05 15:20:04 -0500

    context listener

---
+++ globalsettings/MyServletContextListener.java
@@ -0,0 +1,25 @@
+package !!!PROJECT_NAME!!!.modules;
+import !!!PROJECT_NAME!!!.ctrlrs.*;
+import com.google.common.base.*;
+import com.google.common.collect.*;
+import com.google.inject.*;
+import com.google.inject.servlet.*;
+import com.mob.web.*;
+import java.io.*;
+import java.lang.reflect.*;
+import java.net.URL;
+import java.util.*;
+import java.util.regex.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import org.apache.log4j.Logger;
+
+public @Singleton class MyServletContextListener extends GuiceServletContextListener {
+
+	//@Override
+	protected Injector getInjector() {
+		Injector injector = Guice.createInjector( new MainServletModule(), new AppConfigModule() );
+		return injector;
+	}
+
+}
