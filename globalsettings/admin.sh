68f54a525f5d61b43c864a3efbe2e6d77129bc0b
mike o'brien <mike@meetup.com>
2009-12-11 22:38:55 -0500

    updated admin script

--- globalsettings/admin.sh
+++ globalsettings/admin.sh
@@ -2,5 +2,5 @@
 if [ "$1" = "runserver" ]; then
 java -cp lib/*:WebContent/WEB-INF/classes/:./conf/ com.mob.bootstrap.Jetty
 elif [ "$1" = "models" ]; then
-java -cp lib/*:./build com.mob.orm.DbInspector ./src/!!!PROJECT_NAME!!!/models !!!PROJECT_NAME!!!.models
+java -cp lib/*:./build com.mob.orm.DbInspector ./conf/settings.properties ./src/!!!PROJECT_NAME!!!/models !!!PROJECT_NAME!!!.models
 fi
